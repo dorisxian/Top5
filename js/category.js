009eddfeab20f09d7b076353c14225da0a804eef
dorisxian <Doris@Doriss-MacBook-Pro.local>
2014-03-25 22:51:10 -0400

    minor updates, create style.less used for global styles

--- js/category.js
+++ js/category.js
@@ -1,41 +1,39 @@
-$(document).ready(function(){
-
-  $('.t5-ranking-box').hover(function () {
-    var currentclass = $(this).attr('class');
-    var specific = ".";
-    specific += currentclass.slice(15);
-    specific += "-pic";
-    $("li").siblings(".t5-pic").addClass('hidden');
-    $(specific).removeClass('hidden');
-  });
-
-  // init Isotope
-  var $feed = $('#feed').isotope({
-    itemSelector: '.list-item',
-    layoutMode: 'vertical',
-    filter: '*',
-    vertical: {
-      horizontalAlignment: 0.5,
-    }
-  });
-
-  // filter items when filter link is clicked
-  $('#filters button').click(function(){
-    var filterValue = $(this).attr('data-filter');
-    $feed.isotope({ 
-      filter: filterValue
+/*jslint browser: true*/
+/*global $, jQuery*/
+$(document).ready(function () {
+    "use strict";
+    $('.t5-ranking-box').hover(function () {
+        var specific = "." + $(this).attr('class').slice(15) + "-pic";
+        $("li").siblings(".t5-pic").addClass('hidden');
+        $(specific).removeClass('hidden');
     });
 
-  });
+    // init Isotope
+    var $feed = $('#feed').isotope({
+        itemSelector: '.list-item',
+        layoutMode: 'vertical',
+        filter: '*',
+        vertical: {
+            horizontalAlignment: 0.5
+        }
+    });
 
-  $('#filters button').click(function(){
-    var $this = $(this);
-    // don't proceed if already selected
-    if ( $this.hasClass('active') ) {
-      return false;
-    }
-    $('#filters').find('.active').removeClass('active');
-    $this.addClass('active'); 
-  });
+    // filter items when filter link is clicked
+    $('#filters button').click(function () {
+        var filterValue = $(this).attr('data-filter');
+        $feed.isotope({
+            filter: filterValue
+        });
+    });
+
+    $('#filters button').click(function () {
+        var $this = $(this);
+        // don't proceed if already selected
+        if ($this.hasClass('active')) {
+            return false;
+        }
+        $('#filters').find('.active').removeClass('active');
+        $this.addClass('active');
+    });
 
 });
