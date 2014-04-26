d28d36c3aefb234af63ce79ee3d3044d7f37769d
Lyra Le Yin <yinle.leyin@gmail.com>
2014-04-26 13:55:08 -0400

    Add to items.json while adding a new list

--- svc/list_adding.php
+++ svc/list_adding.php
@@ -12,11 +12,23 @@
 	
 	$num = array_push($json, $_POST);
 	
-	(array)$json[$num - 1]['id'] = $lastId + 1;
+	$new = (array)$json[$num - 1];
+	$new['id'] = $lastId + 1;
+	
 	
 	$output = "../data/lists.json";
 	file_put_contents($output, json_encode($json));
-	
 
+
+	// save to items.json
+	
+	$input = "../data/items.json";
+	$json = json_decode(file_get_contents($input));
+	foreach($new['items'] as $item){
+		array_push($json, $item['title']);
+	}	
+	$output = "../data/items.json";
+	file_put_contents($output, json_encode($json));
+	
 
 ?>
