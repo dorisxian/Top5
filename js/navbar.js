e9bf6905d8ad605a8ba314c23a2b65f6a31b8ebb
Lyra Le Yin <yinle.leyin@gmail.com>
2014-04-26 19:24:23 -0400

    Navbar login. redirect to index.html when log out form profile page

---
+++ js/navbar.js
@@ -0,0 +1,20 @@
+	$('#t5-logout').click(function(){
+		$.removeCookie('username');
+		if (window.location.href.search('user-profile') === -1 ) {
+			window.location.reload();
+		} else {
+			window.location.href = 'index.html';			
+		}
+	});
+
+
+      $('#t5-navbar-login-btn').click(function(){
+      	var username = $('#t5-navbar-login-username').val();
+      	var password = $('#t5-navbar-login-password').val();
+      	if (username === 'Dolly' & password === 'Dolly') {
+      		$.cookie("username", "Dolly");
+			window.location.reload();
+      	} else {
+      		alert("Username or password incorrect, please try again.");
+      	}
+      });
