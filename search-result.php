844c0b496f47a562c8aaf70d535c04a96250bd21
Lyra Le Yin <yinle.leyin@gmail.com>
2014-04-28 22:24:56 -0400

    Search result.

--- search-result.php
+++ search-result.php
@@ -29,7 +29,7 @@
 
     <!-- Category Menu -->
     <section id='t5-cat-menu'></section>
-    <h3><span class="highlight">Share</span> and <span class="highlight">discover</span> Top 5 things in your life, 
+    <h3 class='t5-headline'><span class="highlight">Share</span> and <span class="highlight">discover</span> Top 5 things in your life, 
     or inspired by awesome people!</h3>
     <div class="row">
 	    <section class="col-md-9 search-result centered">
@@ -39,8 +39,8 @@
 		            <p class="h2">Search Result</p>
 		        </div>
 		        <div class="col-sm-8 col-md-8">
-			        <form class="t5-search-box" action="/search-result.php" method="POST">
-		                <input type="text" name="keyword" class="form-control typeahead" placeholder="Search for people, lists, things, etc..." value="<?php echo $_POST['keyword']; ?>">
+			        <form class="t5-search-box" action="search-result.php" method="POST">
+		                <input id='t5-search-input-2' type="text" name="keyword" class="form-control typeahead" placeholder="Search for people, lists, things, etc..." value="<?php echo $_POST['keyword']; ?>">
 		                <button type="submit"></button>
 		            </form>
 		        </div>
@@ -48,17 +48,17 @@
 			<div class="row t5-result">
 				<ul class="nav nav-tabs" id="result-tabs">
 					<li class="t5-tab-title"><a href="#result-people">People (0)</a></li>
-					<li class="t5-tab-title active"><a href="#result-lists">Lists (3)</a></li>
+					<li class="t5-tab-title active"><a href="#result-lists">Lists (<span id='t5-lists-num'>0</span>)</a></li>
 					<li class="t5-tab-title"><a href="#result-items">List Items (7)</a></li>
 				</ul>
 				<div class="tab-content">
-					<div class="tab-pane fade in" id="result-people">
+					<div class="tab-pane in" id="result-people">
 						<div class="col-md-10 centered">
 							<p class="t5-no-result">No matches</p>
 						</div>
 					</div>
-					<div class="tab-pane fade active" id="result-lists">lists</div>
-					<div class="tab-pane fade" id="result-items">items</div>
+					<div class="tab-pane active" id="result-lists"></div>
+					<div class="tab-pane " id="result-items"> To be implemented.</div>
 				</div>
 			</div>
 	      
@@ -78,6 +78,7 @@
     <script src="js/min/search-ck.js" type="text/javascript"></script>
     -->
     <!-- Page js -->
+    <script src="js/list.js" type="text/javascript"></script>
     <script>
 	
 	$(document).ready(function () {
@@ -96,6 +97,23 @@
 			e.preventDefault();
 			$(this).tab('show');
 		});
+
+		
+		var ky = $('#t5-search-input-2').val();
+		//alert(ky);
+  		$.getJSON("data/lists.json", function(listsData){
+  			var i = 0;
+  			$.each(listsData, function(index, listData){
+  				if (listData.title.search(new RegExp(ky, 'i')) !== -1) {
+ 	     			var list = new $.top5.List(listData, $('#result-lists'));
+					i++;  					
+  				}
+  			});
+  			$('#t5-lists-num').html(i);
+  		});
+		
+		
+		
 	});
     </script>
   </body>
